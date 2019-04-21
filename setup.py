fd82151512f9680f71e708f3e1dc76475bd7cc7f
yutiansut <yutiansut@qq.com>
2019-04-21 19:27:30 +0800

    #

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize(['QAAccount.pyx','QAParameter.pyx'],        # Cython code file with primes() function  # Python code file with primes_python_compiled() function
+    ext_modules=cythonize(['QAAccount.pyx','QAOrder.pyx'],        # Cython code file with primes() function  # Python code file with primes_python_compiled() function
                           annotate=True),        # enables generation of the html annotation file
 )
